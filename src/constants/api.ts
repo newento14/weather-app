5f132037bd338af46c0c825d03e94a772911ff1a
Yuriy <yurii.choboda@gmail.com>
2023-11-18 14:35:54 +0200

    add env variables

--- src/constants/api.ts
+++
@@ -1,2 +0,0 @@
-export const API_KEY:string = '';
-export const API_URL:string = 'https://api.openweathermap.org/data/2.5/';
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
 import {ICords, IWeather} from "../types/weatherTypes.ts";
-import {API_KEY, API_URL } from "../constants/api.ts";
 
 export class WeatherApi {
   static async get5DaysForecast(cords: ICords) {
-    const response = await axios.get<IWeather>(API_URL + `forecast?lat=${cords.lat}&lon=${cords.lon}&appid=${API_KEY}&units=metric`);
+    const response = await axios.get<IWeather>(process.env.VITE_API_URL + `forecast?lat=${cords.lat}&lon=${cords.lon}&appid=${process.env.VITE_API_KEY}&units=metric`);
     return response.data;
   }
 
   static async getCurrentWeather(cords: ICords) {
-    const response = await axios.get<IWeather>(API_URL + `weather?lat=${cords.lat}&lon=${cords.lon}&appid=${API_KEY}&units=metric`);
+    const response = await axios.get<IWeather>(process.env.VITE_API_URL + `weather?lat=${cords.lat}&lon=${cords.lon}&appid=${process.env.VITE_API_KEY}&units=metric`);
     return response.data;
   }
 }
