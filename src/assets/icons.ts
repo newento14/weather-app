42add7320c61eeed40a4a436b8a145e7b43745bf
Yuriy <yurii.choboda@gmail.com>
2023-11-18 14:40:07 +0200

    fixes

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,14 +1,25 @@
+// @ts-ignore
 import SunnyIcon from './weather/sunny.svg'
+// @ts-ignore
 import CloudyIcon from './weather/cloudy.svg'
+// @ts-ignore
 import NightRainIcon from './weather/night-rain.svg'
+// @ts-ignore
 import SunnyRainIcon from './weather/sunny-rain.svg'
+// @ts-ignore
 import NightIcon from './weather/moon.svg'
+// @ts-ignore
 import SunnyCloudyIcon from './weather/sunny-cloudy.svg'
+// @ts-ignore
 import CloudyWindIcon from './weather/cloudy-wind.svg'
+// @ts-ignore
 import SnowIcon from './weather/snow.svg'
+// @ts-ignore
 import MistIcon from './weather/mist.svg'
+// @ts-ignore
 import SunnyThunderstormIcon from './weather/sunny-thunder.svg'
-import NightThunderstormIcon from "./weather/night-thunder.svg"
+// @ts-ignore
+import NightThunderstormIcon from './weather/night-thunder.svg'
 
 export {
     SunnyIcon,
